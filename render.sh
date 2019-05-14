f48c2840bd624f69a10ba2b71f03b926cbb98b17
kjarmicki <jarmicki@gmail.com>
2019-05-14 22:20:44 +0200

    render fields

--- render.sh
+++ render.sh
@@ -1,5 +1,20 @@
+renderFieldValue() {
+  value=$1
+  case $value in
+    "${boardFieldEmpty}")
+      echo "☐"
+      ;;
+    "${boardFieldX}")
+      echo "X"
+      ;;
+    "${boardFieldO}")
+      echo "O"
+      ;;
+  esac
+}
+
 renderStatus() {
-  echo "Now placing: ${playerCurrentField}"
+  echo "Now placing: $(renderFieldValue $playerCurrentField)"
   echo ""
 }
 
@@ -7,11 +22,12 @@ renderBoard() {
   for ((y=0; y<$boardHeight; y++)); do
     for ((x=0; x<$boardHeight; x++)); do
       value=$(boardGetFieldValue $x $y)
+      renderedValue=$(renderFieldValue $value)
       format=""
       if [ $x -eq "${cursorPosition[0]}" ] && [ $y -eq "${cursorPosition[1]}" ]; then
         format="\e[7m"
       fi
-      echo -ne "${format}${value}\e[0m"
+      echo -ne "${format}${renderedValue}\e[0m"
       if [ $x -eq $((boardWidth - 1)) ]; then
         echo ""
       fi
